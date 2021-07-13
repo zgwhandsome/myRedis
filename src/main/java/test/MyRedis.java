a1e80cb743771a3ce20649878d8fcb99d1544425
controll <2846012154@qq.com>
2021-07-13 12:20:40 +0800

    endEdite

---
+++ src/main/java/test/MyRedis.java
@@ -0,0 +1,16 @@
+package test;
+
+import redis.clients.jedis.Jedis;
+
+public class MyRedis {
+    public static void main(String[] args) {
+        /*Jedis jedis = new Jedis("192.168.174.250",6379);
+        jedis.auth("20010522ZGW");
+        jedis.set("name","14");
+        System.out.println(jedis.get("name"));
+        System.out.println(jedis.flushDB());*/
+        System.out.println("zhangguow ");
+        System.out.println("ddddddddd");
+        String string = new String("zhang");
+    }
+}
